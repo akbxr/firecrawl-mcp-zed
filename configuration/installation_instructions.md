f1d47cdac48264fdb35d65b8b41f20a89c417c6a
Bennet Bo Fenner <bennet@zed.dev>
2025-05-04 17:25:26 +0200

    Implement context server configuration API

---
+++ configuration/installation_instructions.md
@@ -0,0 +1,1 @@
+To use Firecrawl's MCP, go to your [account settings](https://www.firecrawl.dev/app/api-keys) and generate an API key.
--- src/mcp_server_firecrawl.rs
+++ src/mcp_server_firecrawl.rs
@@ -1,14 +1,17 @@
+use schemars::JsonSchema;
 use serde::Deserialize;
 use std::env;
 use zed::settings::ContextServerSettings;
-use zed_extension_api::{self as zed, serde_json, Command, ContextServerId, Project, Result};
+use zed_extension_api::{
+    self as zed, serde_json, Command, ContextServerConfiguration, ContextServerId, Project, Result,
+};
 
 const PACKAGE_NAME: &str = "firecrawl-mcp";
 const SERVER_PATH: &str = "node_modules/firecrawl-mcp/dist/index.js";
 
 struct FirecrawlModelContextExtension;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, JsonSchema)]
 struct FirecrawlContextServerSettings {
     firecrawl_api_key: String,
 }
@@ -45,6 +48,25 @@ impl zed::Extension for FirecrawlModelContextExtension {
                 .to_string()],
             env: vec![("FIRECRAWL_API_KEY".into(), settings.firecrawl_api_key)],
         })
+    }
+
+    fn context_server_configuration(
+        &mut self,
+        _context_server_id: &ContextServerId,
+        _project: &Project,
+    ) -> Result<Option<ContextServerConfiguration>> {
+        let installation_instructions =
+            include_str!("../configuration/installation_instructions.md").to_string();
+        let default_settings = include_str!("../configuration/default_settings.jsonc").to_string();
+        let settings_schema =
+            serde_json::to_string(&schemars::schema_for!(FirecrawlContextServerSettings))
+                .map_err(|e| e.to_string())?;
+
+        Ok(Some(ContextServerConfiguration {
+            installation_instructions,
+            default_settings,
+            settings_schema,
+        }))
     }
 }
 
